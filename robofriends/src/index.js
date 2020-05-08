4f7c722ab15286f4bd08a0d8d0a04cae4bd3d294
megamareep <megamareep@gmail.com>
2020-05-08 14:15:26 +0100

    roboFriends

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import App from './containers/App'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting={'Hello333'+'React Ninja'}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
